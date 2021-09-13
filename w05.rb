120582b0d89656d708b83dff7679ebdddf43540f
dani <dani.zaghian@gmail.com>
2021-09-13 12:05:23 -0700

    adds decomp for week 5

---
+++ w05.rb
@@ -0,0 +1,13 @@
+# Write a function that takes in two strings and returns the longest common substring.
+
+# clarify
+# example input => "girlfriend" "friendly"
+# expected output => "friend"
+# example input => "together" "bothersome"
+# expected output => "ther"
+
+# visualize
+# Write a function that prints out "hello"
+# Write a function that takes in 2 strings and prints each
+# Write a function that takes in 2 strings and loops through each letter of the first string and prints each out
+# Write a function that takes in 2 strings and loops through each letter of the first string, then compares each letter from the loop to each letter in the second string (print "MATCH!" if they match)
