ca140087db767ade952539165bdad02f567e0346
dani <dani.zaghian@gmail.com>
2021-09-13 13:02:14 -0700

    decomposition update

--- w05.rb
+++ w05.rb
@@ -11,3 +11,16 @@
 # Write a function that takes in 2 strings and prints each
 # Write a function that takes in 2 strings and loops through each letter of the first string and prints each out
 # Write a function that takes in 2 strings and loops through each letter of the first string, then compares each letter from the loop to each letter in the second string (print "MATCH!" if they match)
+
+
+# Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
+
+# Clarify
+# example input => [2, 4, 7, -9, 2, 7, 2]
+# expected output => [2, 4, 7, -9]
+
+# Decomposition
+# Write a function that takes in an array of numbers and prints it
+# Write a function that takes in an array of numbers and prints each number one at a time
+# Write a function that takes in an array of numbers and compares the first number with each other number in the array, and prints "match" if they match
+# Write a function that takes in an array of numbers and loops through each number, and runs an inner loop to compare the outer loop number with the inner loop number and print "match" if they match
