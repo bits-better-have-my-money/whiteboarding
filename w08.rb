b96d9b4f58a179761dd7cf9edec9d6e2e85cc597
dani <dani.zaghian@gmail.com>
2021-10-05 13:32:10 -0700

    adds w08

---
+++ w08.rb
@@ -0,0 +1,13 @@
+# Write a function that takes in a string of code with parentheses, square brackets, and curly brackets, and checks if the code is balanced with correct bracket syntax.
+
+# example input => "(x + [1, 2, 7, {a: 2, b:6}])"
+# expected output => true
+# example input => "(x + [1, {a: 2, b:6])}"
+# expected output => false
+
+# Write a function that takes in a string and prints out the string
+# Write a function that takes in a string and prints out each letter one at a time
+# Write a function that takes in a string and counts the number of open parenthesis there are in the string
+# Write a function that takes in a string and counts the number of open parenthesis and closing parenthesis there are in the string
+# Write a function that takes in a string and counts the number of open parenthesis/curly braces/square brackets and closing there are in the string
+# Write a function that takes in a string of code with parentheses, square brackets, and curly brackets and checks if there is a closing parenthese, square bracket, or curly bracket for each opening
